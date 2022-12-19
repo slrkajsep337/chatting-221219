4042a2d9d67c7c776ac10f49d88cfc71c8f1928a
이수진 <slrkajsep337@naver.com>
2022-12-19 23:34:09 +0900

    Initial commit

---
+++ src/main/java/com/example/networkprac/NetworkPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.networkprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkPracApplication.class, args);
+    }
+
+}
